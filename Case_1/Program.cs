41e0e8d55ac911d47a86f7b235a24f6bb230a767
Kainen <Andkor92@mail.ru>
2022-09-21 05:16:30 +0300

    Added code for case 1

---
+++ Case_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] NewArrayOfStringsWhoseLengthIsLessThanOrEqualNumberOfCharacters(string[] array, int numberOfCharacters)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) { if (array[i].Length > 0 && array[i].Length <= numberOfCharacters) { count++; } }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++) { if (array[i].Length > 0 && array[i].Length <= numberOfCharacters) { newArray[j] = array[i]; j++; } }
+    return newArray;
+}
+
+Console.WriteLine("Введите элементы массива, разделяя их запятой и пробелом (', '): ");
+string[] input = Console.ReadLine().Split(", ").ToArray();
+
+string[] output = NewArrayOfStringsWhoseLengthIsLessThanOrEqualNumberOfCharacters(input, 3);
+
+Console.WriteLine();
+Console.WriteLine("Ответ: ");
+Console.WriteLine("[" + string.Join((", "), output) + "]");
